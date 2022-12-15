200bb168730937826829c8dd92b822bbe09b6f7f
Laurel Elm <laurel.elm@gmail.com>
2022-12-15 10:17:30 -0700

    Update AboutScreen.js with LoadingComponent

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,32 +2,7 @@ import { Text, ScrollView } from 'react-native';
 import { Card, ListItem, Avatar } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-
-const AboutScreen = () => {
-    const partners = useSelector((state) => state.partners);
-
-    return (
-        <ScrollView>
-            <Mission />
-            <Card>
-                <Card.Title>Community Partners</Card.Title>
-                <Card.Divider />
-                {partners.partnersArray.map((partner) => {
-                    return (
-                        <ListItem key={partner.id}>
-                            <Avatar source={{ uri: baseUrl + item.image }} rounded />
-                            <ListItem.Content>
-                                <ListItem.Title>{partner.name}</ListItem.Title>
-                                <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
-                            </ListItem.Content>
-                        </ListItem>
-                    )
-                }
-                )}
-            </Card>
-        </ScrollView>
-    )
-}
+import Loading from '../components/LoadingComponent';
 
 const Mission = () => {
     return (
@@ -45,7 +20,59 @@ const Mission = () => {
                 visited with each other.
             </Text>
         </Card>
-    )
+    );
 }
+
+const AboutScreen = () => {
+    const partners = useSelector((state) => state.partners);
+
+    if (partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Loading />
+                </Card>
+            </ScrollView>
+        );
+    }
+
+    if (partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Text>{partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+
+    return (
+        <ScrollView>
+            <Mission />
+            <Card>
+                <Card.Title>Community Partners</Card.Title>
+                <Card.Divider />
+                {partners.partnersArray.map((partner) => {
+                    return (
+                        <ListItem key={partner.id}>
+                            <Avatar source={{ uri: baseUrl + partner.image }} rounded />
+                            <ListItem.Content>
+                                <ListItem.Title>{partner.name}</ListItem.Title>
+                                <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
+                            </ListItem.Content>
+                        </ListItem>
+                    )
+                }
+                )}
+            </Card>
+        </ScrollView>
+    );
+};
 
 export default AboutScreen;

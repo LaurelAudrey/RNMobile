252f0b381286ecf666a632d50096bf3099958474
Laurel Elm <laurel.elm@gmail.com>
2022-12-16 09:39:13 -0700

    Add form to rating modal, update RenderCommentItem to show stars

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,4 +1,5 @@
-import { FlatList, StyleSheet, Text, View, Button, Modal, Pressable } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Button, Modal } from 'react-native';
+import { Rating, Input } from 'react-native-elements';
 import { useState } from 'react';
 import RenderCampsite from '../features/campsites/RenderCampsite';
 import { useSelector, useDispatch } from 'react-redux';
@@ -11,14 +12,39 @@ const CampsiteInfoScreen = ({ route }) => {
     const favorites = useSelector((state) => state.favorites);
 
     const [showModal, setShowModal] = useState(false);
+    const [rating, setRating] = useState(5);
+    const [author, setAuthor] = useState('');
+    const [text, setText] = useState('');
 
     const dispatch = useDispatch();
+    
+    const handleSubmit = () => {
+        const newComment = {
+            author,
+            rating,
+            text,
+            campsiteId: campsite.id
+        };
+        console.log(newComment);
+        setShowModal(!showModal);
+    }
+
+    const resetForm = () => {
+        setAuthor('');
+        setRating(5);
+        setText('');
+    }
 
     const renderCommentItem = ({ item }) => {
         return (
             <View style={styles.commentItem}>
                 <Text style={{ fontSize: 14 }}>{item.text}</Text>
-                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Rating 
+                    read-only 
+                    startingValue={item.rating}
+                    imageSize={10}
+                    style={{ alignItems: 'flex-start', paddingVertical: '5%' }} 
+                />
                 <Text style={{ fontSize: 12 }}>
                     {`-- ${item.author}, ${item.date}`}
                 </Text>
@@ -57,13 +83,46 @@ const CampsiteInfoScreen = ({ route }) => {
             onRequestClose={() => setShowModal(!showModal)}
         >
             <View style={styles.modal}>
+                <Rating 
+                    showRating
+                    startingValue={rating}
+                    imageSize={40}
+                    onFinishRating={(rating) => setRating(rating)}
+                    style={{ paddingVertical: 10 }}
+                />
+                <Input 
+                    placeholder='Author'
+                    leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                    leftIconContainerStyle={{ paddingRight: 10 }}
+                    onChangeText={(author) => setAuthor(author)}
+                    value={author}
+                />
+                <Input 
+                    placeholder='Comment'
+                    leftIcon={{ type: 'font-awesome', name: 'comment-o' }}
+                    leftIconContainerStyle={{ paddingRight: 10 }}
+                    onChangeText={(text) => setText(text)}
+                    value={text}
+                />
+                <View style={{ margin: 10 }}>
+                    <Button 
+                        title='Submit'
+                        color='#5637DD'
+                        onPress={() => {
+                            handleSubmit();
+                            resetForm();
+                        }}
+                    />
+                </View>
                 <View style={{ margin: 10 }}>
                     <Button
-                        onPress={() => setShowModal(!showModal)}
-                        color='#808080'
                         title='Cancel'
-                    >
-                    </Button>
+                        color='#808080'
+                        onPress={() => {
+                            setShowModal(!showModal);
+                            resetForm();
+                        }}
+                    />
                 </View>
             </View>
         </Modal>

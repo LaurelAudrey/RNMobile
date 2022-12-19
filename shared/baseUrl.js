2026b942732561f1631660b9af786cf0ba7e1fef
Laurel Elm <laurel.elm@gmail.com>
2022-12-18 19:46:42 -0700

    Add alternate IP addresses for Home + Work

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,5 @@
-export const baseUrl = 'http://192.168.1.116:3001/';
+// Good Times 
+// export const baseUrl = 'http://192.168.1.116:3001/';
+
+//Home
+export const baseUrl = 'htt[://192.168.39.133:3001/';

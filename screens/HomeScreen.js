4ebff133daa5a8a643a1132a7ba32159f90b2efc
Laurel Elm <laurel.elm@gmail.com>
2022-12-19 17:37:51 -0700

    HomeScreen Animation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,5 @@
-import { ScrollView, Text, View } from 'react-native';
+import { useEffect, useRef } from 'react';
+import { Text, View, Animated } from 'react-native';
 import { Card } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -6,11 +7,10 @@ import Loading from '../components/LoadingComponent';
 
 const FeaturedItem = (props) => {
     const { item } = props;
-    
+
     if (props.isLoading) {
         return <Loading />;
     }
-
     if (props.errMess) {
         return (
             <View>
@@ -18,7 +18,6 @@ const FeaturedItem = (props) => {
             </View>
         );
     }
-
     if (item) {
         return (
             <Card containerStyle={{ padding: 0 }}>
@@ -46,6 +45,12 @@ const HomeScreen = () => {
     const campsites = useSelector((state) => state.campsites);
     const promotions = useSelector((state) => state.promotions);
     const partners = useSelector((state) => state.partners);
+    const scaleValue = useRef(new Animated.Value(0)).current;
+    const scaleAnimation = Animated.timing(scaleValue, {
+        toValue: 1,
+        duration: 1500,
+        useNativeDriver: true
+    });
 
     const featCampsite = campsites.campsitesArray.find((item) => item.featured);
     const featPromotion = promotions.promotionsArray.find(
@@ -53,24 +58,28 @@ const HomeScreen = () => {
     );
     const featPartner = partners.partnersArray.find((item) => item.featured);
 
+    useEffect(() => {
+        scaleAnimation.start();
+    }, []);
+
     return (
-        <ScrollView>
-            <FeaturedItem 
+        <Animated.ScrollView style={{ transform: [{ scale: scaleValue }] }}>
+            <FeaturedItem
                 item={featCampsite}
                 isLoading={campsites.isLoading}
-                errMess={campsites.errMess} 
+                errMess={campsites.errMess}
             />
-            <FeaturedItem 
-                item={featPromotion} 
+            <FeaturedItem
+                item={featPromotion}
                 isLoading={promotions.isLoading}
                 errMess={promotions.errMess}
             />
-            <FeaturedItem 
+            <FeaturedItem
                 item={featPartner}
                 isLoading={partners.isLoading}
-                errMess={partners.errMess} 
+                errMess={partners.errMess}
             />
-        </ScrollView>
+        </Animated.ScrollView>
     );
 };
 
